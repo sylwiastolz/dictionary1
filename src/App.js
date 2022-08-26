ba90d4bc2c306ed3189b56f79c87c34b625233ba
sylwiastolz <sylwiastolz@interia.pl>
2022-08-26 18:01:27 +0200

    install react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import logo from "./logoss.png";
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="btn btn-primary">Hellllo</p>
+        <img src={logo} alt="logosylwia" />
+      </header>
+    </div>
+  );
+}
