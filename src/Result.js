1a2c332edb923b4c7b3f7b1597f6dd74078661bc
sylwiastolz <sylwiastolz@interia.pl>
2022-08-28 19:21:49 +0200

    phonetic

--- src/Result.js
+++ src/Result.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
 
 export default function Result(props) {
@@ -7,6 +7,14 @@ export default function Result(props) {
     return (
       <div>
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
